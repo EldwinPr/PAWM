f1a6474a5a966bfe3d89866077e0870948b0a237
Eldwin Pradipta <EldwinPradipta670@gmail.com>
2024-11-13 21:11:33 +0700

    a

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 const config = {
     API_URL: 'https://pylearn-backend.vercel.app'
 };
+
+// Make sure config is available globally
+window.config = config;
+// If using ES modules, add:
+export default config;
